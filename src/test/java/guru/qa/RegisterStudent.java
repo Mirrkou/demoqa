2aaba58cc658cfba7ffc162b375e27c4edffb801
Mirka <miragaleeva8@gmail.com>
2021-10-28 17:33:39 +0300

    filed deleted

--- src/test/java/guru/qa/RegisterStudent.java
+++ src/test/java/guru/qa/RegisterStudent.java
@@ -14,41 +14,39 @@ import org.junit.jupiter.api.Test;
 
 public class RegisterStudent {
 
-	@BeforeAll
-	static void beforeAll() {
-		Configuration.startMaximized = true;
-	}
+    @BeforeAll
+    static void beforeAll() {
+        Configuration.startMaximized = true;
+    }
 
-	@BeforeEach
-	void openUrl() {
-		open("https://demoqa.com/automation-practice-form");
-	}
+    @BeforeEach
+    void openUrl() {
+        open("https://demoqa.com/automation-practice-form");
+    }
 
-	@DisplayName("Регистрация студента")
-	@Test
-	void registerStudent() {
-		$("#firstName").setValue("Mirka");
-		$("#lastName").setValue("Mendeleeva");
-		$("#userEmail").setValue("test@gmail.com");
-		$("[for='gender-radio-2']").click();
-		$("#userNumber").setValue("9111111111");
-		$("#dateOfBirthInput").click();
-		$(".react-datepicker__month-select").selectOptionByValue("5");
-		$(".react-datepicker__year-select").selectOptionByValue("1987");
-		$$(".react-datepicker__day").find(text("11")).click();
-		$("#subjectsInput").setValue("Arts").pressEnter();
-		$("[for='hobbies-checkbox-3']").click();
-		$("#uploadPicture").uploadFile(new File("src/resources/123.png"));
-		$("#currentAddress").setValue("Big city");
-		$("#react-select-3-input").setValue("NCR").pressEnter();
-		$("#react-select-4-input").setValue("Delhi").pressEnter();
-		$("#submit").click();
+    @DisplayName("Регистрация студента")
+    @Test
+    void registerStudent() {
+        $("#firstName").setValue("Mirka");
+        $("#lastName").setValue("Mendeleeva");
+        $("#userEmail").setValue("test@gmail.com");
+        $("[for='gender-radio-2']").click();
+        $("#userNumber").setValue("9111111111");
+        $("#dateOfBirthInput").click();
+        $(".react-datepicker__month-select").selectOptionByValue("5");
+        $(".react-datepicker__year-select").selectOptionByValue("1987");
+        $$(".react-datepicker__day").find(text("11")).click();
+        $("#subjectsInput").setValue("Arts").pressEnter();
+        $("[for='hobbies-checkbox-3']").click();
+        $("#uploadPicture").uploadFile(new File("src/resources/123.png"));
+        $("#currentAddress").setValue("Big city");
+        $("#react-select-3-input").setValue("NCR").pressEnter();
+        $("#react-select-4-input").setValue("Delhi").pressEnter();
+        $("#submit").click();
 
-		$(".table-responsive").shouldHave(text("Mirka Mendeleeva"), text("test@gmail.com"),
-// @formatter:off
-										  text("Female"), text("9111111111"), text("11 June,1987"),
-										  text("Arts"), text("Music"), text("123.png"),
-										  text("Big city"), text("NCR Delhi"));
-// @formatter:on
-	}
+        $(".table-responsive").shouldHave(text("Mirka Mendeleeva"), text("test@gmail.com"),
+                                          text("Female"), text("9111111111"), text("11 June,1987"),
+                                          text("Arts"), text("Music"), text("123.png"),
+                                          text("Big city"), text("NCR Delhi"));
+    }
 }
